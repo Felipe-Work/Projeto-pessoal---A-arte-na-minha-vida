4d425b7d4587223eaf0ff74634b65ad7f948db58
Felipe-Work <felipe.maia@sptech.school>
2026-05-22 01:05:02 -0300

    algumas alterações de questionário e banco

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -56,6 +56,7 @@ function cadastrar(req, res) {
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
     var cpf = req.body.cpfServer;
+    var nota_antes = req.body.nota_antesServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -69,7 +70,7 @@ function cadastrar(req, res) {
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, cpf)
+        usuarioModel.cadastrar(nome, email, senha, cpf, nota_antes)
             .then(
                 function (resultado) {
                     res.json(resultado);
